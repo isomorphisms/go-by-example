280b92f3e0728ea30186756014617fc95e0d58a5
isomorphisms <isomorphisms@sdf.org>
2018-09-03 03:44:37 +0000

    typo

--- lesson-06-if-else.go
+++ lesson-06-if-else.go
@@ -15,8 +15,8 @@ func main() {
 
   if num := 9; num < 0 {
     fmt.Println(num, "is negative")
-  } else if num > 10 {
-    fmt.Println(num, "has 1 digit")
+  } else if num < 10 {
+    fmt.Println(num, "has one digit")
   } else {
     fmt.Println(num, "has multiple digits")
   }
